21783b4a9460e5b6a96b9b4f984cd7eaa6033a62
Christopher LaPointe <cmlapointe11@gmail.com>
2017-07-25 22:29:52 -0400

    First pass at currency lister

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = require('rc')('gdax', {
+	passphrase: '',
+	key: '',
+	b64secret: '',
+	url: 'https://api.gdax.com',
+});
---
+++ index.js
@@ -0,0 +1,34 @@
+const gdax = require('gdax');
+const blessed = require('blessed');
+const numeral = require('numeral');
+const Promise = require('bluebird');
+const _ = require('lodash');
+const config = require('./config');
+
+const client = new gdax.AuthenticatedClient(config.key, config.b64secret, config.passphrase, config.url);
+
+const screen = blessed.screen({smartCSR: true});
+const priceTable = blessed.table({
+	parent:screen,
+	border: {
+	    type: 'line'
+	  },
+});
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+function update() {
+	client.getAccounts((err, resp, data) => {
+		const NUMF = "0,0.0000";
+		let rows = _.map(data, val => {
+			return [val.currency, numeral(val.balance).format(NUMF), numeral(val.available).format(NUMF), numeral(val.hold).format(NUMF)];
+		});
+		rows.unshift(['Currency', 'Balance', 'Available', 'Hold']);
+		priceTable.setData(rows);
+		screen.render();
+	});
+}
+
+setInterval(update, 30 * 1000);
+update();
